e61d5915afe2b55f9a670a5e91070327ce0c39ee
Antoine <AntoineFr@users.noreply.github.com>
2019-07-13 14:21:13 +0200

    Permit use of different fonts from FA

--- extend.php
+++ extend.php
@@ -6,8 +6,10 @@ use s9e\TextFormatter\Configurator;
 
 return (new Extend\Formatter)
     ->configure(function (Configurator $config) {
-        $config->BBCodes->addCustom(
-			'[FA]{IDENTIFIER}[/FA]', '<i class="fas fa-{IDENTIFIER}"></i>'
-		);
+        $config->BBCodes->addCustom('[FA]{IDENTIFIER}[/FA]', '<i class="fas fa-{IDENTIFIER}"></i>'); // for compatibility reasons
+        $config->BBCodes->addCustom('[FAS]{IDENTIFIER}[/FAS]', '<i class="fas fa-{IDENTIFIER}"></i>');
+		$config->BBCodes->addCustom('[FAR]{IDENTIFIER}[/FAR]', '<i class="far fa-{IDENTIFIER}"></i>');
+		$config->BBCodes->addCustom('[FAL]{IDENTIFIER}[/FAL]', '<i class="fal fa-{IDENTIFIER}"></i>');
+		$config->BBCodes->addCustom('[FAB]{IDENTIFIER}[/FAB]', '<i class="fab fa-{IDENTIFIER}"></i>');
     })
 ;
